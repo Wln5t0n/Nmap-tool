a0342b0eb3f32e314edd9fd22a0a1105c4a263be
Comradesec <85233203+Comradesec@users.noreply.github.com>
2021-06-02 19:01:56 +0530

    Add files via upload
    python tool to scan htb machines.

---
+++ htbs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+from os import system as cmd
+
+print(" \n Nmap scanning tool by 'comradesec' follow on github. \n\n")
+
+if len(sys.argv)==2:
+        
+        ip = ('10.10.10.'+sys.argv[1]) 
+        
+        cmd('nmap '+ip+' -oA normalscan')
+       
+        cmd("cat normalscan.nmap | grep open | awk -F/ '{print $1}' ORS=',' | rev | cut -c 2- | rev > opened-ports.txt")
+       
+        f=open("opened-ports.txt", "r")
+        ports = f.read()
+        print("\nOPENED PORTS:")
+        print(ports)
+        
+        cmd('nmap -sC -sV '+ip+' -p'+ports)
+        
+        cmd('rm opened-ports.txt normalscan.gnmap normalscan.xml normalscan.nmap')
+
+else:
+        sys.stderr.write("Usage: {0} <last digits of IP>\n\n".format(sys.argv[0]))
+        
+print("\n\n follow me on:- https://github.com/Comradesec")
